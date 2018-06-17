bef6709df80d4917c5b46eb3d843ac9596083c29
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2018-06-17 20:22:42 +0530

    Initialcommit

---
+++ src/Pathfinding/Assets/Scripts/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    [SerializeField]
+    LayerMask unwalkableMask;
+
+    [SerializeField]
+    Vector2 gridWorldSize;
+
+    [SerializeField]
+    float nodeSize;
+
+    Node[,] grid;
+
+    int gridCountX, gridCountY;
+
+	// Use this for initialization
+	void Start () {
+        gridCountX = Mathf.RoundToInt(gridWorldSize.x / nodeSize);
+        gridCountY = Mathf.RoundToInt(gridWorldSize.y / nodeSize);
+
+        CreateGrids();
+    }
+
+    private void CreateGrids()
+    {
+        // store grid world position and walkable (bool)
+        // to fill the array
+
+        // calculate the bottom left of the world
+        Vector3 worldBottomLeftPosition = transform.position
+                                          - (Vector3.left * (gridWorldSize.x / 2))
+                                          - (Vector3.back * (gridWorldSize.y / 2));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+    }
+}
